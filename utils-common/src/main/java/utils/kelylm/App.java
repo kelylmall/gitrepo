b123e99f80436d0d21d55da3f1e527b79404f559
kelylmall@gmail.com <kelylmall@gmail.com>
2015-04-27 18:14:33 +0800

    update on dev branch

--- utils-common/src/main/java/utils/kelylm/App.java
+++ utils-common/src/main/java/utils/kelylm/App.java
@@ -5,7 +5,7 @@ package utils.kelylm;
  */
 public class App {
     public static void main(String[] args) {
-
+        System.out.printf("测试");
         System.out.println("Hello World!");
     }
 }
